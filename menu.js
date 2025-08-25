bdb40f04fe25f15929663bf9453a3072e763dcf4
mgmn <mgmn@gft.com>
2025-08-25 13:47:16 +0200

    se agregan los viejos ficheros para el scanner mediante aikido security

---
+++ menu.js
@@ -0,0 +1,16 @@
+﻿// =================================================================
+// ARCHIVO JAVASCRIPT MODIFICADO PARA UN LOGOUT LIMPIO
+// =================================================================
+
+// --- PASO 1: Añadimos la nueva variable global ---
+var isLoggingOut = false; // Bandera para controlar el logout
+
+var lastSelection = "";
+
+function toggleSelection(selection) {
+    if (lastSelection == false && lastSelection != "" && lastSelection != selection) {
+        $(lastSelection).hide();
+    }
+    $("." + selection).toggle();
+    lastSelection = selection;
+}
